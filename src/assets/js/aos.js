0294f51faf975339863e2df254262ffa11beb08c
exclusiveyalta <83545285+exclusiveyalta@users.noreply.github.com>
2021-09-28 20:41:19 +0300

    aos edit and svg bg

--- src/assets/js/aos.js
+++ src/assets/js/aos.js
@@ -14,7 +14,7 @@ AOS.init({
 
 
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    offset: 80, // offset (in px) from the original trigger point
+    offset: 100, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 1300, // values from 0 to 3000, with step 50ms
     easing: 'ease', // default easing for AOS animations
