f3d9dcd715817b57b7ba1ab9a8e1269c300cea3a
Daniil Ilyin <d.v.ilyin@tinkoff.ru>
2025-08-24 20:10:30 +0300

    old page /yachts.html redirect to home, others to /boats/

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
+  // временные редиректы, пока поисковые системы не раздуплятся
   async redirects() {
     return [
       {
-        source: "/:path.html",
+        source: "/:path((?!yachts).*)\\.html",
         destination: "/boats/:path",
+        permanent: true,
+      },
+      {
+        // https://yahta-yalta.ru/yachts.html
+        source: "/yachts.html",
+        destination: "/",
         permanent: true,
       },
     ]
