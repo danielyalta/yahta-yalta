acddb332a6e81cdf8c2f0cd2ad00bf255219b0e5
Daniil Ilyin <d.v.ilyin@tinkoff.ru>
2025-08-24 13:05:21 +0300

    responsive for 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ import { ButtonLink } from "@/components/ui/button-link"
 const NotFoundPage = () => {
   return (
     <div className="from-darkblue to-darkblue2 relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-gradient-to-br font-sans text-white">
-      <div className="relative z-10 flex flex-col items-center p-8 text-center">
+      <div className="relative z-10 flex flex-col items-center p-8 pt-[100px] text-center">
         <div className="mb-8 flex items-center justify-center space-x-4 text-9xl font-extrabold tracking-tight text-white drop-shadow-lg">
           <span className="text-blue-800">4</span>
           <div className="relative flex h-24 w-24 items-center justify-center rounded-full border-8 border-white bg-blue-500 shadow-xl">
@@ -17,11 +17,11 @@ const NotFoundPage = () => {
           <span className="text-blue-800">4</span>
         </div>
 
-        <h1 className="mb-4 text-5xl leading-tight font-bold drop-shadow-2xl">
+        <h1 className="mb-4 text-3xl leading-tight font-bold drop-shadow-2xl sm:text-5xl">
           Затерялись в открытом море...
         </h1>
 
-        <p className="text-opacity-90 mb-8 max-w-lg text-lg text-white">
+        <p className="text-opacity-90 mb-8 max-w-lg text-white sm:text-lg">
           Мы не смогли найти путь к этой странице. Похоже, она сбилась с курса и
           ушла в неизвестность.
         </p>
